f06843bbaf346b630f7164e26a11bc58074ed743
Samisateurr <samuel.baranowski@orange.fr>
2024-08-03 19:13:51 +0200

    fix : test

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.scss';
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contactez-nous</h1>
+      <h1>Contactez-nouss</h1>
       <p>Pour toute question, veuillez nous contacter à...</p>
     </div>
   );
